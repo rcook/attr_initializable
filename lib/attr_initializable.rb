1aa84a1cd36e943665f5ed0ef4065256fd954208
Richard Cook <rcook@rcook.org>
2013-12-08 14:17:24 -0800

    Implement Rails 4 support
    * Move mass_assignment_authorizer inside "included" block
    * Check for presence of "protected_attributes" gem
    * Issue warning if mass assignment sanitization is not strict
    * Add "protected_attributes" gem to Rails 4 Gemfile

--- lib/attr_initializable.rb
+++ lib/attr_initializable.rb
@@ -3,6 +3,16 @@ module AttrInitializable
 
   module ClassMethods
     def attr_initializable(attributes, options = {})
+      unless respond_to?(:protected_attributes)
+        raise RuntimeError.new('`attr_initializable` relies on `attr_accessible`. ' \
+           'Please add `protected_attributes` to your Gemfile to use `attr_initializable`.')
+      end
+
+      unless Rails.configuration.active_record.mass_assignment_sanitizer == :strict
+        logger.warn "WARNING: Running with strict mass assignment sanitizer is recommended.\n" \
+          "Please add config.active_record.mass_assignment_sanitizer = :strict to config/application.rb.\n"
+      end
+
       role = options.delete(:as) || :default
       initializable_attributes[role] ||= []
       initializable_attributes[role] += Array(attributes)
@@ -14,12 +24,11 @@ module AttrInitializable
   end
 
   included do
-  end
-
-  def mass_assignment_authorizer(role = :default)
-    authorizer = super
-    authorizer += Array(self.class.initializable_attributes[role]) unless persisted?
-    authorizer
+    def mass_assignment_authorizer(role = :default)
+      authorizer = super
+      authorizer += Array(self.class.initializable_attributes[role]) unless persisted?
+      authorizer
+    end
   end
 end
  
